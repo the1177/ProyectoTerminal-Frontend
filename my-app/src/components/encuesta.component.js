8f7a9a096b54832db7e2dbc3fd53776729fce39b
EDREYSPC\user <160300135@ucaribe.edu.mx>
2021-11-10 08:59:57 -0600

    Se agregaron los botones de votar

--- my-app/src/components/encuesta.component.js
+++ my-app/src/components/encuesta.component.js
@@ -12,10 +12,10 @@ export default class Quiz extends Component {
                             <div class ="image">
                                 <div class ="containerIma">
                                     <div class ="image1">
-                                        <img src="FG.jpg" alt="imagen"/>
+                                        <img class="photo" src="FG.jpg" alt="imagen"/>
                                     </div>
                                     <div class ="image2">
-                                        <img src="wz.jpg" alt="imagen"/>
+                                        <img class="photo" src="wz.jpg" alt="imagen"/>
                                     </div>    
                                 </div>
                                 <div class ="ima-change">
@@ -23,14 +23,42 @@ export default class Quiz extends Component {
                                         <h5 className="text-change">
                                             ¿No lo conozces?
                                         </h5>
+                                        <button type="submit" className="changeButton btn btn-primary btn-block">Cambiar</button>
                                     </div>
                                     <div class ="change-two">
                                         <h5 className="text-change-two">
                                             ¿No lo conozces?
                                         </h5>
+                                        <button type="submit" className="changeButton btn btn-primary btn-block">Cambiar</button>
+                                        
                                     </div>
                                 </div>                                  
-                            </div>                          
+                            </div>
+                            <div class ="vote">
+                                <div class ="first-opinion">
+                                    Me identifico más con: "Fallguys"
+                                </div>
+                                <div class ="container-botton">
+                                    <div>
+                                        <button type="submit" className="vote1-botton btn btn-block"></button>
+                                    </div>
+                                    <div>
+                                        <button type="submit" className="vote2-botton btn btn-block"></button>
+                                    </div>
+                                    <div>
+                                        <button type="submit" className="vote3-botton btn btn-block"></button>
+                                    </div>
+                                    <div>
+                                        <button type="submit" className="vote4-botton btn btn-block"></button>
+                                    </div>
+                                    <div>
+                                        <button type="submit" className="vote5-botton btn btn-block"></button>
+                                    </div>
+                                </div>
+                                <div class ="second-opinion">
+                                    Me identifico más con: "Warzone"
+                                </div>
+                            </div>                         
                     </div>               
                 </div>            
         );
