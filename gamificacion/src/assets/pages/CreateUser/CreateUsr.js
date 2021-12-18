bc2693c16f4c2b8943233af0d38432c6113b3881
DESKTOP-KMBNDR0\edre_ <160300135@ucaribe.edu.mx>
2021-12-18 12:12:18 -0500

    Responsive Create user

--- gamificacion/src/assets/pages/CreateUser/CreateUsr.js
+++ gamificacion/src/assets/pages/CreateUser/CreateUsr.js
@@ -1,17 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import "./CreateUsr.css"
 import Title from "./components/Title/Title";
 import imageRegister from './components/images/registerImage.jpg'
-import Media from 'react-media'
+import Input from './components/Input/Input'
 
 const CreateUser = () =>{
     return(
-        <div className="Container-father-register">
-            <Media query="(max-width:600px)">
-                {matches=>{
-                    return matches ? "s": "world";
-                }}    
-            </Media>            
+        <div className="Container-father-register">         
             <div className='Container-Image'>
                 <img className='image-register' src={imageRegister}/>
             </div>
@@ -19,10 +14,78 @@ const CreateUser = () =>{
                 <div className="sign-up-register">
                     <Title text='Registro'/>
                 </div>
-                
+                <div className="container-form">
+                    <div className="input-container">
+                        <form>
+                            <div>
+                                <label htmlFor="nombre">Nombre:</label>
+                            </div>
+                            <div>
+                                <Input 
+                                attribute={{
+                                    id:'nombre',
+                                    name:'nombre',
+                                    type: 'text',
+                                    placeholder:'Nombre'
+                                }}
+                                />
+                            </div>
+                        </form>
+                    </div>
+                    <div className="input-container">
+                        <form>
+                            <div>
+                                <label htmlFor="apellido">Apellido:</label>
+                            </div>
+                            <div>
+                            <Input 
+                                attribute={{
+                                    id:'apellido',
+                                    name:'apellido',
+                                    type: 'text',
+                                    placeholder:'Apellido'
+                                }}
+                                />
+                            </div>
+                        </form>
+                    </div>
+                    <div className="input-container">
+                        <form>
+                            <div>
+                                <label htmlFor="email">Email:</label>
+                            </div>
+                            <div>
+                            <Input 
+                                attribute={{
+                                    id:'email',
+                                    name:'email',
+                                    type: 'email',
+                                    placeholder:'Ingresa tu email'
+                                }}
+                                />
+                            </div>
+                        </form>
+                    </div>
+                    <div className="input-container">
+                        <form>
+                            <div>
+                                <label htmlFor="password">Contraseña:</label>
+                            </div>
+                            <div>
+                            <Input 
+                                attribute={{
+                                    id:'password',
+                                    name:'password',
+                                    type: 'password',
+                                    placeholder:'Ingresa tu contraseña'
+                                }}
+                                />
+                            </div>
+                        </form>
+                    </div>
+                </div> 
             </div>
         </div>
-
     )
 };
 
