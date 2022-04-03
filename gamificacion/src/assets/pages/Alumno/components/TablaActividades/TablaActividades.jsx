7e878526a8f4605fcacd900efc8324b6ec2fbe8e
DESKTOP-KMBNDR0\edre_ <160300135@ucaribe.edu.mx>
2022-04-03 15:01:24 -0500

    Scroll tabla y actualizaciones

--- gamificacion/src/assets/pages/Alumno/components/TablaActividades/TablaActividades.jsx
+++ gamificacion/src/assets/pages/Alumno/components/TablaActividades/TablaActividades.jsx
@@ -8,12 +8,17 @@ export default function TablaActividades(){
     const [select, setselect] = useState('');
     //custom data
     var dataa = [
-     { id: 1, actividad: 'Realizar un server en minecraft',  state: 'Por Hacer' }, 
-    { id: 2, actividad: 'Formar equipos de 4 integrantes',  state: 'Realizado' }, 
-    { id: 3, actividad: 'Descargar roblox',  state: 'Por Hacer' }, 
-    { id: 4, actividad: 'Utilizar un microfono en clase' , state: 'Realizado' }, 
-    { id: 5, actividad: 'Tener una exposicion de no mas de 5 minutos',  state: 'No realizada' }, 
-    { id: 6, actividad: 'Crear una dinamica', state: 'No realizada' }];
+    { id: 1, actividad: 'Realizar un server en minecraft',  status: 'Realizado' }, 
+    { id: 2, actividad: 'Formar equipos de 4 integrantes',  status: 'Realizado' }, 
+    { id: 3, actividad: 'Descargar roblox',  status: 'Realizado' }, 
+    { id: 4, actividad: 'Utilizar un microfono en clase' , status: 'Realizado' }, 
+    { id: 5, actividad: 'Tener una exposicion de no mas de 5 minutos',  status: 'Realizado' }, 
+    { id: 6, actividad: 'Crear una dinamica', status: 'Realizado' },
+    { id: 7, actividad: 'Hacer equipos', status: 'No realizada' },
+    { id: 8, actividad: 'Traer una comida', status: 'Realizado' },
+    { id: 9, actividad: 'Traer una presentación', status: 'Por hacer' },
+    { id: 10, actividad: 'Ganar una partida de fortnite', status: 'Por hacer' },
+    { id: 11, actividad: 'Tener una comida en el salon', status: 'Por hacer' }];
     //Select onchage function, getting option selected value and save inside state variable
     function handleChange (e){
         //set state variable with option value
@@ -24,11 +29,11 @@ export default function TablaActividades(){
     useEffect(() => {
         setData(select);
         if(select == 'select Grade'){
-            data = dataa.filter(dataa => dataa.state !== select); 
+            data = dataa.filter(dataa => dataa.status !== select); 
             setData(data);
         }else{
                     //Doing filteration on according to select state option
-        data = dataa.filter(dataa => dataa.state === select);   
+        data = dataa.filter(dataa => dataa.status === select);   
         }
         //set state variable data after filteration
         setData(data);
@@ -57,13 +62,13 @@ export default function TablaActividades(){
                 </thead>
                 <tbody>
                 { // calling state variable data to filter data inside table
-                data.map(function({id, actividad, state}){
+                data.map(function({id, actividad, status}){
                 
                     return (
                         <tr>
                         <td>{id}</td>
                         <td>{actividad}</td>                    
-                        <td>{state}</td>
+                        <td>{status}</td>
                     </tr>
                         );
                     })}
