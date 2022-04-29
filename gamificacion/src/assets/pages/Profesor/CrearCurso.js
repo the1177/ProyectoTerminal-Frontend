9b6bf94d0498fde68c312d98f1110157fefe8114
aimee <160300115@ucaribe.edu.mx>
2022-04-29 15:59:51 -0500

    Cambios Aimee CrearCurso

--- gamificacion/src/assets/pages/Profesor/CrearCurso.js
+++ gamificacion/src/assets/pages/Profesor/CrearCurso.js
@@ -1,242 +1,280 @@
-import {React, useState} from 'react';
+import React from 'react';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import MuiDrawer from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import TextareaAutosize from '@mui/base/TextareaAutosize';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
+import ClassIcon from '@mui/icons-material/Class';
+
+import { 
+  Container, 
+  Box, 
+  Grid, 
+  Paper, 
+  Typography, 
+  TextField,
+  TextFieldProps, 
+  Button } from '@mui/material';
 import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Link from '@mui/material/Link';
-import chroma from 'chroma-js';
-import Select, { StylesConfig } from 'react-select';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import { mainListItems, secondaryListItems } from './components/Listitems/listitems';
-import { StyledEngineProvider } from '@mui/material/styles';
-
-
-
-import Upload from './components/Buttons/upload';
-import InputAuto from './components/Buttons/inputauto';
-
 import Menu from './components/Menu/Menu';
 import NavBar from './components/NavBar/NavBar';
+import * as Yup from 'yup';
+
+import { 
+  Formik,
+  FormikConsumer,
+  Form,
+  useField,
+  Field,
+  FormikProps,
+  ErrorMessage,
+  FieldArray,
+  FastField, 
+} from 'formik';
 
 const drawerWidth = 240;
 
 const CssTextField = styled(TextField)({
-  '& label.Mui-focused': {
-    color: 'purple',
-  },
-  '& .MuiInput-underline:after': {
-    borderBottomColor: 'purple',
-  },
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderColor: 'light-green',
+    '& label.Mui-focused': {
+      color: 'purple',
     },
-    '&:hover fieldset': {
-      borderColor: 'blue',
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'purple',
     },
-    '&.Mui-focused fieldset': {
-      borderColor: 'purple',
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'light-green',
+      },
+      '&:hover fieldset': {
+        borderColor: 'blue',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'purple',
+      }
     },
-  },
 });
+
+{/* UseField TextField*/}
+type FormikTextFieldProps = {
+  formikKey: string,
+} & TextFieldProps
+
+export const FormikTextField = ({ formikKey, ...props }: FormikTextFieldProps) => {
+  const [field, meta, helpers] = useField(formikKey);
+  return <CssTextField
+      id={field.name}
+      name={field.name}
+      helperText={meta.touched ? meta.error : ""}
+      error={meta.touched && Boolean(meta.error)}
+      value={field.value}
+      onChange={field.onChange}
+      {...props}
+  />
+}
 
 const mdTheme = createTheme();
 
-function CrearCursoContent() {
+function getWeeksAfter(date, amount) {
+  return date ? addWeeks(date, amount) : undefined;
+}
+
+const CursoContent = () => {
     const saved = localStorage.getItem("user");
     const user = JSON.parse(saved);
-
-    //const savedCursos = localStorage.getItem("cursos");
-    var cursosString = '{ "cursos":[] }';
-    var objCursos = JSON.parse(cursosString);
-
-    //const [inputs, setInputs] = useState({});
-    const [inputs, setInputs] = useState({});
-    const [open, setOpen] = useState(true);
-
-    const handleChange = (event) => {
-      const name = event.target.name;
-      const value = event.target.value;
-      setInputs(values => ({...values, [name]: value}));
-    }
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log(inputs);
-      const curso = inputs;
-      objCursos['cursos'].push(curso);
-      cursosString = JSON.stringify(objCursos);
-      console.log(objCursos);
-    }
-
     console.log(user);
 
-    //const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = React.useState(true);
     const toggleDrawer = () => {
       setOpen(!open);
     };
+
+    const [value, setValue] = React.useState(new Date());
   
     return (
-      <ThemeProvider theme={mdTheme}>
-        <Box sx={{ display: 'flex' }}>
-          <CssBaseline />
-          
-          <NavBar tituloNavBar="Crear Curso" open={ open } setOpen={ setOpen }/>
+      <React.Fragment>
+        <ThemeProvider theme={mdTheme} >
+          <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            
+            <NavBar tituloNavBar="Crear Curso" open={ open } setOpen={ setOpen }/>
 
-          <Menu user={ user }  open ={ open } setOpen={ setOpen }/>
+            <Menu user={ user }  open ={ open } setOpen={ setOpen }/>
+            
+            {/* Paper es la sección completa del Form cuadro blanco*/}
+            <Paper
+              fixed
+              sx={{
+                p: 2,
+                margin: 'auto',
+                flexDirection: 'column',
+                borderRadius: 3,
+                height: 'auto',
+                width: '50%',
+                alignItems: 'center',
+                alignContent: 'center',
+                //textAlign:'center',
+                marginTop: 15,
+              }}
+            >
+              {/* Box es todo el contenido del form */}
+              <Box
+                fixed
+                component="form"
+                sx={{
+                  marginTop: 8,
+                  '& .MuiTextField-root': { m: 1, width: '100%'},
+                  my: 3,
+                  mx: 1,
+                  //bgcolor: 'pink',
+                  height: 'auto',
+                  minWidth: '100%',
+                }}
 
-          <Box
-            component="main"
-            sx={{
-              backgroundcolor: (theme) =>
-                theme.palette.mode === 'light'
-                  ? theme.palette.grey[100]
-                  : theme.palette.grey[900],
-              flexGrow: 1,
-              height: '100vh',
-              overflow: 'auto',
-            }}
-          >
-            <Toolbar />
-            <Container maxWidth="lg" sx={{ mt: 5, mb: 5 }}>
-
-                <Grid container justifyContent="center" rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid item xs={'auto'}>
-                        <Paper
-                            sx={{
-                            p: 2,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            borderRadius: 3,
-                            height: 'auto',
-                            
-                            }}
-                        >
-                            <Box
-                            component="form"
-                            sx={{
-                                '& .MuiTextField-root': { m: 1, width: '90ch' },
-                                my: 3,
-                                mx: 7
-                            }}
-                            noValidate
-                            autoComplete="off"
-                            onSubmit={handleSubmit}
-                            >
-
-                                <Grid item xs>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                    Crear Curso
-                                    </Typography>
-                                </Grid>
-                                
-
-                                <div>
-                                    <Divider light variant="h7" textAlign="left">Información del Curso</Divider>
-                                    <Box sx={{ m: 2 }}>
-
-                                        <CssTextField
-                                          required 
-                                          id="outlined-required" 
-                                          label="Nombre del Curso" 
-                                          variant="outlined"
-                                          name="nombreCurso"
-                                          value={inputs.nombreCurso || ""}
-                                          onChange={handleChange} 
-                                        />
-
-                                    </Box>
-
-                                    <Divider light variant="h7" textAlign="left">Detalles del Curso</Divider>
-                                    <Box sx={{ m: 2 }}>
-
-
-                                        <CssTextField
-                                          required 
-                                          id="outlined" 
-                                          label="ID del Curso" 
-                                          variant="outlined"
-                                          name="idCurso"
-                                          value={inputs.idCurso || ""}
-                                          onChange={handleChange} 
-                                        />
-
-                                    </Box>
-
-                                    <Box sx={{ m: 2 }}>
-
-                                        <CssTextField
-                                          required 
-                                          id="outlined-required" 
-                                          label="Sección del Curso" 
-                                          variant="outlined"
-                                          name="seccionCurso"
-                                          value={inputs.seccionCurso || ""}
-                                          onChange={handleChange} 
-                                        />
-
-                                    </Box>
-
-                                    <Box sx={{ m: 2 }}>
-
-                                        <CssTextField
-                                          required 
-                                          id="outlined-required" 
-                                          label="Areá del Curso" 
-                                          variant="outlined"
-                                          name="areaCurso"
-                                          value={inputs.areaCurso || ""}
-                                          onChange={handleChange} 
-                                        />
-
-                                    </Box>
-
-                                    <Box sx={{ m: 2 }}>
-
-                                      <CssTextField
-                                        id="outlined-multiline-static"
-                                        label="Descripción"
-                                        multiline
-                                        rows={4}
-                                        defaultValue=""
-                                        name="descripcionCurso"
-                                        value={inputs.descripcionCurso || ""}
-                                        onChange={handleChange} 
-                                      />
-
-                                    </Box>
-
-
-                                    <input type="submit" />
-                                </div>
-                            </Box>
-                        </Paper>
-                    </Grid>
+              >
+                <Grid item xs>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Crear Curso
+                  </Typography>
                 </Grid>
-                
-            </Container>
+
+                <Grid item xs>
+                    <Box
+                      component="form"
+                      sx={{
+                        '& .MuiTextField-root': { m: 1, width: '100%'},
+                        margin: 'auto',
+                        padding: 1,
+                        minWidth: '100%',
+                      }}
+
+                    >
+
+                      <Formik
+                        initialValues={{
+                          nombreCurso: '',
+                          idCurso: '',
+                          seccionCurso: '',
+                          areaCurso: '',
+                          descripcionCurso: '',
+                        }}
+                        validationSchema={Yup.object({
+                          nombreCurso: Yup.string()
+                            .max(50, 'Must be 50 characters or less')
+                            .required('Required'),
+                          idCurso: Yup.string()
+                            .max(15, 'Must be 15 characters or less')
+                            .required('Required'),
+                          seccionCurso: Yup.number()
+                            .required('Required'),
+                          areaCurso: Yup.string()
+                            .max(50, 'Must be 50 characters or less')
+                            .required('Required'),
+                          descripcionCurso: Yup.string()
+                            .max(300, 'Must be 300 characters or less')
+                            .required('Required'),
+                        })}
+                        onSubmit={async(values, { setSubmitting }) => {
+                          setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            console.log(value);
+                            setSubmitting(false);
+                          }, 400);
+                        }}
+                      >
+                        {({ values }) => (
+                          <Form>
+                            <Divider light variant="h7" textAlign="left">Información del Curso</Divider>
+                            <Box sx={{ m: 2,  /*bgcolor:'red'*/}}>
+                              <FormikTextField formikKey="nombreCurso" 
+                                label="Nombre del Curso"
+                                variant="outlined"
+                                name="nombreCurso"
+                                placeholder="Nombre del Curso"
+                                type="text"
+                              />
+                            </Box>
+
+                            <Divider light variant="h7" textAlign="left">Detalles del Curso</Divider>
+                            <Box sx={{ m: 2, /*bgcolor:'red'*/}}>
+                              <FormikTextField formikKey="idCurso" 
+                                label="ID del Curso"
+                                variant="outlined"
+                                name="idCurso"
+                                placeholder="ID del Curso"
+                                type="text"
+                              />
+                            </Box>
+
+                            <Box sx={{ m: 2,  /*bgcolor:'red'*/}}>
+                              <FormikTextField formikKey="seccionCurso" 
+                                label="Sección del Curso"
+                                variant="outlined"
+                                name="seccionCurso"
+                                placeholder="Sección del Curso"
+                                type="number"
+                              />
+                            </Box>
+
+                            <Box sx={{ m: 2, /*bgcolor:'red'*/}}>
+                              <FormikTextField formikKey="areaCurso" 
+                                label="Areá del Curso"
+                                variant="outlined"
+                                name="areaCurso"
+                                placeholder="Areá del Curso"
+                                type="text"
+                              />
+                            </Box>
+
+                            <Box sx={{ m: 2,  /*bgcolor:'red'*/}}>
+                              <FormikTextField formikKey="descripcionCurso" 
+                                label="Descripción del Curso"
+                                variant="outlined"
+                                multiline
+                                rows={4}
+                                name="descripcionCurso"
+                                placeholder="Descripción del Curso"
+                                type="text"
+                              />
+                            </Box>     
+
+                            <Box item sx={{m: 2, textAlign:'center'}}>
+                              <Button type="submit" variant="contained" endIcon={<ClassIcon />}>Crear Curso</Button>
+                            </Box>
+
+                            <FormikConsumer>
+                              {({ validationSchema, validate, onSubmit, ...rest }) => (
+                                <pre
+                                  style={{
+                                    fontSize: '.85rem',
+                                    padding: '.25rem .5rem',
+                                    overflowX: 'scroll',
+                                  }}
+                                >
+                                  {JSON.stringify(rest, null, 2)}
+                                </pre>
+                              )}
+                            </FormikConsumer>
+
+                          </Form>
+
+                        )}
+                      </Formik>
+                      
+
+                      
+                    </Box>
+                </Grid>
+
+              </Box>
+
+            </Paper>
+
           </Box>
-        </Box>
-      </ThemeProvider>
+        </ThemeProvider>
+
+      </React.Fragment>
     );
 }
 
-
-
 export default function CrearCurso() {
-    return <CrearCursoContent />;
+    return <CursoContent />;
 }
+
+
